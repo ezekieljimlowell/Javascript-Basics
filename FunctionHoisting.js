927bc7289cd050e44e3e55cdfd4a0716cb553401
ezekieljimlowell <88152947+ezekieljimlowell@users.noreply.github.com>
2022-02-16 12:26:30 +0530

    Changes in FunctionHoisting

--- FunctionHoisting.js
+++ FunctionHoisting.js
@@ -3,10 +3,10 @@ function add(a,b) {
     return a+b;
 }
 
-console.log(subtract(3,89));
+//console.log(subtract(3,89)); //subtract is not a function
 var subtract = function subtract(a,b) {
     return b-a;
 }
 
-console.log(multiply(3,4))
-const multiply = (a,b) => a*b;
+//console.log(multiply(3,4)) //can't access lexical declaration 'multiply' before initialization for let and const 
+var multiply = (a,b) => a*b; //for var multiply is not a function
