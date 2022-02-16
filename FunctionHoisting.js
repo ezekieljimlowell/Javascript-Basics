20268df8e2a084178f44d45b4fb212f492a2eb5f
ezekieljimlowell <88152947+ezekieljimlowell@users.noreply.github.com>
2022-02-16 12:22:30 +0530

    adding function hoisting

---
+++ FunctionHoisting.js
@@ -0,0 +1,12 @@
+console.log(add(2,5));
+function add(a,b) {
+    return a+b;
+}
+
+console.log(subtract(3,89));
+var subtract = function subtract(a,b) {
+    return b-a;
+}
+
+console.log(multiply(3,4))
+const multiply = (a,b) => a*b;
