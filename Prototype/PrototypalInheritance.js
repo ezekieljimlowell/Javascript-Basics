1edd9cde003fda90705af7b1884385bb4914521b
ezekieljimlowell <88152947+ezekieljimlowell@users.noreply.github.com>
2022-02-17 16:44:05 +0530

    updates in prototypal inheritance

--- Prototype/PrototypalInheritance.js
+++ Prototype/PrototypalInheritance.js
@@ -58,3 +58,35 @@ console.log(obj.saturn === obj.__proto__.saturn, obj);
 const arr = [1,2,3,3,4,3];
 console.log(arr.__proto__ === Array.prototype);
 console.log(arr.__proto__.__proto__ === Object.prototype);
+
+const words = {
+    0: "engine",
+    1: "train",
+    length: 2
+}
+
+words.join = Array.prototype.join;
+
+console.log(words.join(","))
+
+const train = {
+    electric: "current"
+}
+
+function Train(name1) {
+    this.name1 = name1;
+}
+
+Train.prototype = train;
+
+const train1 = new Train();
+console.log(train1.electric);
+
+function Animal(name5) {
+    this.name5 = name5;
+}
+
+const animal1 = new Animal("Dindikal");
+console.log(animal1.constructor == Animal)
+
+
