55e2a5459d08b27ad93107d5ddf57cbcf0a683ac
ezekieljimlowell <88152947+ezekieljimlowell@users.noreply.github.com>
2022-02-21 15:46:25 +0530

    Event loop in js

---
+++ EventLoop/EventLoop.js
@@ -0,0 +1,12 @@
+const foo = () => console.log("foo");
+
+const bar = () => console.log("bar");
+
+const baz = () => {
+    console.log("I am the first one");
+    setTimeout(bar, 0);
+    new Promise((resolve) => resolve("I am promise")).then(val => console.log(val));
+    foo();
+}
+
+baz();
