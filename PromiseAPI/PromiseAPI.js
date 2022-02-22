dfc1caa8e813306a50656d42f042e8b723a007dd
ezekieljimlowell <88152947+ezekieljimlowell@users.noreply.github.com>
2022-02-22 12:54:45 +0530

    Promise API

--- PromiseAPI/PromiseAPI.js
+++ PromiseAPI/PromiseAPI.js
@@ -2,13 +2,13 @@ Promise.all([
     new Promise(resolve => setTimeout(() => {
         resolve("promise1")
     }, 1000)),
-    new Promise(resolve => setTimeout(() => {
+    new Promise((resolve, reject) => setTimeout(() => {
         resolve("promise2")
     }, 1000)),
     new Promise(resolve => setTimeout(() => {
         resolve("promise3")
     }, 1000))
-]).then(val => val.map(element => console.log(element)));
+]).then(val => val.map(element => console.log(element))).catch(err => console.log(err));
 
 let urls = [
     'https://api.github.com/users/iliakan',
@@ -16,11 +16,24 @@ let urls = [
     'https://api.github.com/users/jeresig'
 ];
 
-let request = urls.map(async (req) => await fetch(req));
+let request = urls.map(req => fetch(req));
 
 Promise.all(request).then(responses => responses.map(response => console.log(response.url, response.status)));
 
+let names = ['iliakan', 'remy', 'jeresig'];
+let request1 = names.map(name => fetch(`https://api.github.com/users/${name}`));
 
+Promise.all(request1).then(responseArray => responseArray.map(data => console.log(data.url, data.status)))
+
+let URL2 = [
+    'https://api.github.com/users/iliakan',
+    'https://api.github.com/users/remy',
+    'http://no-such-url',
+];
+
+let requestArray1 = URL2.map(responseURL => fetch(responseURL));
+
+Promise.allSettled(requestArray1).then(responses => responses.map(response => console.log(response.value.url)).catch(err => console.log(err)));
 
 
 
